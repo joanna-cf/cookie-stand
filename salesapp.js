86e76a01df828de39e5595a7ee46e0ca993ceebd
joannaarroyo <joliuac@gmail.com>
2019-03-11 16:28:28 -0700

    used array to generate numbers for all locations in salesapp.js

--- salesapp.js
+++ salesapp.js
@@ -14,7 +14,7 @@ Calculate sum of hourly totals
 
 */
 
-//Function for randomizing numbers
+//Function for randomizing numbers. Cited: MDN website https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -64,35 +64,45 @@ var alki = {
   location_total: 0
 };
 
+var locations = [pike, seatac, seattleC, capitol, alki];
+var html_headings = ['pike', 'seatac', 'seattleC', 'capitol', 'alki'];
+
 //Function that generates cookies sold per hour, and pushes it into an array
-pike.calculate_hourly_cookies = function(){
-  for (var i = 0; i < hours.length; i++){
-    var customers = getRandomIntInclusive(this.min_customers, this.max_customers);
-    var sold_cookies = customers * this.avg_cookies;
-    console.log(sold_cookies);
-    this.hourly_total.push(sold_cookies);
-    this.location_total = this.location_total + this.hourly_total[i];
-  }
-};
 
-//Create li for each hour, include totals from above
-pike.add_hourly_total_to_page = function(){
-  for (var i = 0; i < hours.length; i++){
-    var pike_list = document.getElementById('pike');
-    var pike_hour_list = document.createElement('li');
-    pike_hour_list.textContent = `${hours[i]}: ${pike.hourly_total[i]}`;
-    pike_list.appendChild(pike_hour_list);
+var calculate_by_location = function (){
+
+  for(var k=0; k < locations.length; k++){
+    locations[k].calculate_hourly_cookies = function(){
+      for (var i = 0; i < hours.length; i++){
+        var customers = getRandomIntInclusive(this.min_customers, this.max_customers);
+        var sold_cookies = customers * this.avg_cookies;
+        console.log(sold_cookies);
+        this.hourly_total.push(sold_cookies);
+        this.location_total = this.location_total + this.hourly_total[i];
+      }
+    };
+
+    //Create li for each hour, include totals from above
+    locations[k].add_hourly_total_to_page = function(){
+      for (var i = 0; i < hours.length; i++){
+        var sold_list = document.getElementById(html_headings[k]);
+        var hour_list = document.createElement('li');
+        hour_list.textContent = `${hours[i]}: ${locations[k].hourly_total[i]}`;
+        sold_list.appendChild(hour_list);
+      }
+    }
+    //Function to add total to
+    locations[k].add_total_to_page = function(){
+      var sold_list = document.getElementById(html_headings[k]);
+      var total = document.createElement('li');
+      total.textContent = 'Total: ' + this.location_total;
+      sold_list.appendChild(total);
+    }
+
+    locations[k].calculate_hourly_cookies();
+    locations[k].add_hourly_total_to_page();
+    locations[k].add_total_to_page();
   }
 }
-//Function to add total to
-pike.add_total_to_page = function(){
-  var pike_list = document.getElementById('pike');
-  var pike_total = document.createElement('li');
-  pike_total.textContent = 'Total: ' + this.location_total;
-  pike_list.appendChild(pike_total);
-}
 
-pike.calculate_hourly_cookies()
-pike.add_hourly_total_to_page();
-pike.add_total_to_page();
-
+calculate_by_location();
